7bf93ca2a8b028bb1432356b15593005bfb41511
Arnav Deshpande <arnavdeshpande222@gmail.com>
2026-06-06 01:10:41 +0530

    spacing

--- backend/app/models/patient.py
+++ backend/app/models/patient.py
@@ -6,6 +6,7 @@ from .medical_condition import MedicalCondition
 from dataclasses import dataclass
 from datetime import datetime
 
+
 @dataclass
 class Patient:
     id: int
@@ -13,9 +14,9 @@ class Patient:
     dob: datetime
     number: str
     is_active: bool
-    last_visit_date: datetime
     total_fees_paid: float
     fees_remaining: float
     visits: list[Visit]
     condition: MedicalCondition
+
 

bfa38bdcd96c56f12578771901eb8a1013343488
Arnav Deshpande <arnavdeshpande222@gmail.com>
2026-06-08 14:03:00 +0530

    Converter.py

--- backend/app/common/converter.py
+++ backend/app/common/converter.py
@@ -9,3 +9,9 @@ def date_to_db_fmt(dt: date | None) -> str | None:
 def date_from_db_fmt(dt: str | None) -> date | None:
     return datetime.strptime(dt, "%m-%d-%Y").date() if dt else None
 
+def date_to_json_fmt(dt: date | None) -> str | None:
+    return dt.strftime("%d-%m-%Y") if dt else None
+
+def date_from_json_fmt(dt: str | None) -> date | None:
+    return datetime.strptime("%d-%m-%Y") if dt else None
+

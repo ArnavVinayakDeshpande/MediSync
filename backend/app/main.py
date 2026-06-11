90b5e61074acbf3dfe655468adf92eaf1fa17787
Arnav Deshpande <arnavdeshpande222@gmail.com>
2026-06-11 15:34:42 +0530

    Added some bugfixes.

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,6 +10,8 @@ from app.database.database import Database
 import app.database.database as db
 from app.managers.patient_manager import PatientManager
 import app.managers.patient_manager as patient_manager
+from app.managers.visit_manager import VisitManager
+import app.managers.visit_manager as visit_manager
 from app.routers.patient import router as patient_router
 from app.routers.visit import router as visit_router
 
@@ -38,6 +40,7 @@ app.include_router(visit_router)
 
 db.database = Database(get_db_path())
 patient_manager.patient_manager = PatientManager(db.database)
+visit_manager.visit_manager = VisitManager(db.database)
 
 @app.get("/")
 def root():
